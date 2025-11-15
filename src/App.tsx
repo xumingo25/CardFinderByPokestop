2f5f2a484697df125458496b2d8a6379c699cfa8
Pobre Domingo <domingo.saavedra.saavedra@gmail.com>
2025-11-14 21:40:20 -0300

    primera versión estable de pokedex

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+interface Pokemon {
+  name: string;
+  id: number;
+  image: string;
+}
 
 function App() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=1100")
+      .then((res) => res.json())
+      .then((data) => {
+        const list: Pokemon[] = data.results.map((p: any) => {
+          const id = p.url.split("/").filter(Boolean).pop();
+          return {
+            name: p.name,
+            id: Number(id),
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+          };
+        });
+
+        setPokemons(list);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pokedex">
+      <div className="pokedex-header">
+        <div className="light red"></div>
+        <div className="light yellow"></div>
+        <div className="light green"></div>
+    </div>
+
+      <div className="pokedex-screen">
+        <div className="pokemon-grid">
+          {pokemons.map((pokemon) => (
+            <a
+              key={pokemon.id}
+              href={`https://pokestop.cl/search/?q=${pokemon.name}`}
+              className="pokemon-tile"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={"https://img.pokemondb.net/sprites/home/normal/" + pokemon.name + ".png"} alt={pokemon.name} />
+             
+            </a>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
